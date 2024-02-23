8878a4944bdc3b5b283d9966a47360557c331043
mar-ieke <marieke.witt@gmail.com>
2024-02-24 00:41:04 +0100

    Texte eingefügt

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => {
   const [error, setError] = useState("");
 
   const handleLogin = () => {
-    if (pass1 === "password1" && pass2 === "password2") {
+    if (pass1 === "pass1" && pass2 === "pass2") {
       setIsLoggedIn(true);
     } else {
-      setError("Satz mit X, das war wohl nix");
+      setError("So werdet ihr den Schatz niemals finden");
     }
   };
 
@@ -23,7 +23,13 @@ const App = () => {
     <div>
       {!isLoggedIn ? (
         <div>
-          <p>Einführung</p>
+          <p>
+            Nicht schlecht, ihr Flachwasserpfeifen seid wahrlich clever! Doch
+            wie steht es um den Teamspirit der Crew? Piratinnen, Piraten,
+            Meerjungfrauen und Wassermänner, nur wo ihr getrennt seid, werdet
+            ihr zusammen weiterkommen! P.S.: Und wisst ihr eigentlich, was das
+            Lieblingsessen von Piraten ist?
+          </p>
           <input
             type="password"
             placeholder="Erstes Passwort"
@@ -38,14 +44,14 @@ const App = () => {
             onChange={(e) => setPass2(e.target.value)}
           />
           <br />
-          <button onClick={handleLogin}>Abtauchen!</button>
+          <button onClick={handleLogin}>Yo ho, a pirate's life for me!</button>
           {error && <p>{error}</p>}
         </div>
       ) : (
         <div>
-          <p>Platzhalter</p>
+          <p>Luftballons können kaputt gehen...</p>
           <Confetti />
-          <button onClick={handleLogout}>Ausloggen</button>
+          <button onClick={handleLogout}>Fertig</button>
         </div>
       )}
     </div>
